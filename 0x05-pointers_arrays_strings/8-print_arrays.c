6bfb97fb503bdc82501e3726ca65b9de25bd411b
hyperbayse <hyperbayse@gmail.com>
2024-03-27 10:14:07 +0100

    update

---
+++ 0x05-pointers_arrays_strings/8-print_arrays.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * 8-print_arrays.c
+ *
+ *  Created on: Mar 26, 2024
+ *      Author: absheriff
+ */
+/**
+ * print_arrays - prints the n numbers of element from an array
+ * @a: passed array
+ * @n: number of elements to be printed
+ * x: iterator
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int x = 0;
+
+	while (x < n)
+	{
+		printf("%d", a[x]);
+
+		if (x != (n - 1))
+			printf(", ");
+		x++;
+	}
+	printf("\n");
+}
