fd785c98cb237f7a624adb99708bd7d185c65658
hyperbayse <hyperbayse@gmail.com>
2024-04-08 16:33:32 +0100

    update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,8 +1,33 @@
+#include "main.h"
 /*
- * 1-strncat.c
+ * 0-strcat.c
  *
- *  Created on: Apr 6, 2024
+ *  Created on: Apr 4, 2024
  *      Author: absheriff
  */
+/**
+ * _strncat - concatenates strings together
+ * @dest: string destination
+ * @src: string source
+ * Return: pointer to the destination
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int x = 0;
 
+	while (dest[x] != '\0')
+		x++;
 
+	while (i < n)
+	{
+		if (src[i] == '\0')
+			break;
+
+		dest[x] = src[i];
+		x++;
+		i++;
+	}
+
+	return (dest);
+}
