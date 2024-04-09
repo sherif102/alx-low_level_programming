220d594a856ed4ffb103723f394632300927d6b9
hyperbayse <hyperbayse@gmail.com>
2024-04-09 11:41:12 +0100

    correction to 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,10 +15,17 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+	int x = 0;
+
+	while (src[x] != '\0')
+		x++;
 
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = src[i];
+		if (i >= x)
+			dest[i] = '\0';
+		else
+			dest[i] = src[i];
 	}
 	return (dest);
 }
