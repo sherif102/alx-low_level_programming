c2e6a245e543da9fbc3e6a5e38d86ead1609253d
hyperbayse <hyperbayse@gmail.com>
2024-04-10 13:09:35 +0100

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,18 +14,29 @@
  */
 int _strcmp(char *s1, char *s2)
 {
+	int r1 = 0;
+	int r2 = 0;
+	int i = 0;
+	int j = 0;
 	int r;
-	int l = *s1;
-	int ri = *s2;
 
-	r = l - ri;
+	while (s1[i] != '\0')
+	{
+		r1 = r1 + s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		r2 = r2 + s2[j];
+		j++;
+	}
 
-	if (l < ri)
-		r = r + 0;
-	else if (l > ri)
-		r = r + 0;
-	else
+	if (r1 > r2)
+		r = r1 - r2;
+	else if (r1 == r2)
 		r = 0;
+	else
+		r = r1 - r2;
 
 	return (r);
 }
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-    char s1[] = "a";
-    char s2[] = "ia";
+    char s1[] = "Hello";
+    char s2[] = "World!";
 
     printf("%d\n", _strcmp(s1, s2));
     printf("%d\n", _strcmp(s2, s1));
