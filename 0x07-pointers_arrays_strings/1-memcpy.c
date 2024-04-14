4c404405020d69325b64863a9528b8dff65276fd
hyperbayse <hyperbayse@gmail.com>
2024-04-14 10:56:06 +0100

    update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,11 +13,12 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
 		dest[i] = src[i];
+		i++;
 	}
 
 	return (dest);
