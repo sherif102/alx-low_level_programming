ad58e0e1bcd65045cf1765f32c0f69dc10478665
hyperbayse <hyperbayse@gmail.com>
2024-04-14 14:15:01 +0100

    correction on 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,13 +13,30 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-
+	unsigned int i = 0, x = 0;
 	unsigned int y = 0;
+	unsigned int j = 0;
 
 	while (accept[y] != '\0')
 		y++;
 
-	y++;
+	while (s[i] != '\0')
+	{
+		while (x < y)
+		{
+			if (s[i] == accept[x])
+			{
+				j++;
+				break;
+			}
+			x++;
+		}
+		if (s[i] != accept[x])
+			break;
 
-	return (y);
+		x = 0;
+		i++;
+	}
+
+	return (j);
 }
