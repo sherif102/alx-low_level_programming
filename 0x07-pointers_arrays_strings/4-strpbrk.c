fcf17834f33787367a4b383d07289d4269eb2088
hyperbayse <hyperbayse@gmail.com>
2024-04-14 15:32:09 +0100

    update on 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/*
+ * 4-strpbrk.c
+ *
+ *  Created on: Apr 14, 2024
+ *      Author: absheriff
+ */
+/**
+ * _strpbrk - searches for set of bits
+ * @s: string to be searched
+ * @accept: where to search
+ * Return: success
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i = 0;
+	unsigned int y = 0;
+	unsigned int j = 0;
+
+	while (accept[y] != '\0')
+		y++;
+
+	while (s[i] != '\0')
+	{
+		while (j < y)
+		{
+			if (s[i] == accept[j])
+				break;
+			j++;
+		}
+		if (s[i] == accept[j])
+			break;
+
+		j = 0;
+		i++;
+	}
+	if (s[i] == accept[j])
+		s = s + i;
+	else
+		s = '\0';
+
+	return (s);
+}
