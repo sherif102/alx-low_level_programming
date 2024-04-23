c1bed48eca6bbc7d91c41ab87fbbecd8001e1dbe
hyperbayse <hyperbayse@gmail.com>
2024-04-23 22:21:46 +0100

    correction on 8-print_diagsum.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -16,8 +16,8 @@ void print_diagsums(int *a, int size)
 {
 	int i;
 	int irev = (size * size) - 1;
-	long unsigned int r1 = 0;
-	long unsigned int r2 = 0;
+	long int r1 = 0;
+	long int r2 = 0;
 
 	for (i = 0; i < (size * size); i = i + (size + 1))
 		r1 = r1 + a[i];
@@ -25,5 +25,5 @@ void print_diagsums(int *a, int size)
 	for (i = (irev - (size - 1) ); i >= (size - 1); i = i - (size - 1))
 		r2 = r2 + a[i];
 
-	printf("%lu, %lu\n", r1, r2);
+	printf("%ld, %ld\n", r1, r2);
 }
