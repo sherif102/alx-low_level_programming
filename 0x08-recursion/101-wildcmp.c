48329b20e20563b832ece14061835ea770729f54
hyperbayse <hyperbayse@gmail.com>
2024-05-11 13:35:18 +0100

    101-wildcmp.c update

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * 101-wildcmp.c
+ *
+ *  Created on: May 9, 2024
+ *      Author: absheriff
+ */
+/**
+ * wildcmp - compare two values and check if they are identical
+ * @s1: the first string
+ * @s2: the second string
+ * Return: 1 if true otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int r;
+	int i = _strlen_recursion(s1);
+	int j = _strlen_recursion(s2);
+
+	if ((s1[0] != s2[0]) && s2[0] != '*')
+		return (0);
+	if ((s2[0] == '*') && (s2[j - 1] == s1[i - 1]))
+		return (1);
+	if (s1[0] == s2[0])
+		r = 1;
+	if (s2[0] == '*' && s2[1] == '\0')
+		return (1);
+	if (s2[0] == '*')
+		return wildcmp(s1, (s2 + 1));
+
+	if ((s1[0] != '\0') || (s2[0] != '\0'))
+		return wildcmp((s1 + 1), (s2 + 1));
+
+	return (r);
+}
--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -1,17 +1,38 @@
+#include "main.h"
 #include <stdio.h>
 
-int is_palindrome(char *);
-
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int r;
+    int r;
 
-	r = is_palindrome("abcddcba");
-	printf("%d\n", r);
-	return (0);
+    r = wildcmp("main.c", "*.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*a*i*n*.*c*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "main.c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "ma********************************c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "*");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "***");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "m.*c");
+    printf("%d\n", r);
+    r = wildcmp("main.c", "**.*c");
+    printf("%d\n", r);
+    r = wildcmp("main-main.c", "ma*in.c");
+    printf("%d\n", r);
+    r = wildcmp("main", "main*d");
+    printf("%d\n", r);
+    r = wildcmp("abc", "*b");
+    printf("%d\n", r);
+    return (0);
 }
