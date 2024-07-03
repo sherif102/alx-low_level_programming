70e1ce8eeb305f9bf1a9df012f564262ab412834
hyperbayse <hyperbayse@gmail.com>
2024-07-03 15:28:36 +0100

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/*
+ * main - the main function
+ * @argc: number of arguments
+ * @argv: arguments array
+ * return: Success
+ */
+
 int main(int argc, char **argv)
 {
-	while(argc--)
+	while (argc--)
 	{
 		printf("%s\n", argv[argc]);
 	}
