31d16b307354cef1b2bf68cedafc7695d259fd81
hyperbayse <hyperbayse@gmail.com>
2024-08-03 12:10:38 +0100

    update

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,62 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strtow - split strings to separate words
+ * @str: supplied string
+ * Return: pointer to the array of split strings
+ */
+
+
+char **strtow(char *str)
+{
+	int i, j;
+	int u = 0;
+	int cc = 0;
+	int x = 0;
+	char **sp;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] != ' ') && (str[i + 1] ==  ' ' || str[i + 1] == '\0'))
+			u++;
+	}
+
+	sp = malloc((u + 1) * sizeof(char *));
+	if (sp == NULL)
+		return (NULL);
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] != ' ') && ((str[i - 1] == ' ') || i == 0))
+		{
+			j = i;
+			for (cc = 0; (str[j] != ' ' || '\0'); cc++)
+				j++;
+
+			sp[x] = malloc((cc + 1) * sizeof(char));
+			if (sp[x] == NULL)
+			{
+				for (i = 0; i < x; i++)
+					free(sp[i]);
+				free(sp);
+				return (NULL);
+			}
+
+			for (j = 0; j < cc; j++, i++)
+				sp[x][j] = str[i];
+			sp[x][j] = '\0';
+			x++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	sp[x] = NULL;
+
+	return (sp);
+}
