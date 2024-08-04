57b6207d355f574f6de80a4820c310a0feea59ce
hyperbayse <hyperbayse@gmail.com>
2024-08-04 14:10:29 +0100

    update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,27 +12,33 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int y;
 	void *np;
 
-	if (new_size > old_size)
-	{
-		np = malloc(new_size);
-		if (np == NULL)
-			return (NULL);
-		for (y = 0; y < old_size ; y++)
-			((char *)np)[y] = ((char *)ptr)[y];
-	}
-	if (new_size <= old_size)
-		return (ptr);
-	if (ptr == NULL)
-	{
-		np = malloc(new_size);
-		if (np == NULL)
-			return (NULL);
-	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
+
+	if (new_size > old_size)
+	{
+		np = malloc(new_size);
+		if (np == NULL)
+			return (NULL);
+
+		for (y = 0; y < old_size ; y++)
+			((char *)np)[y] = ((char *)ptr)[y];
+	}
+	if (new_size <= old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		np = malloc(new_size);
+		if (np == NULL)
+			return (NULL);
+		return (np);
+	}
+
 	free(ptr);
+
 	return (np);
 }
