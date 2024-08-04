21a030dfa4bb2d17d23a17fd784cc8968c939237
hyperbayse <hyperbayse@gmail.com>
2024-08-04 09:22:17 +0100

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int x = 0;
 	char *ap;
 
-	if ((nmemb || size) == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	ap = malloc(nmemb * size);
