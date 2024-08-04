4c7f4dbea8840bb36b741fcd15870a08a318fabc
hyperbayse <hyperbayse@gmail.com>
2024-08-04 09:20:05 +0100

    update

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of space to be allocated
+ * @size: variable size
+ * Return: pointer to the allocated memory or NULL on failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int x = 0;
+	char *ap;
+
+	if ((nmemb || size) == 0)
+		return (NULL);
+
+	ap = malloc(nmemb * size);
+	if (ap == NULL)
+		return (NULL);
+
+	while (x < nmemb)
+	{
+		ap[x] = 0;
+		x++;
+	}
+
+	return (ap);
+}
