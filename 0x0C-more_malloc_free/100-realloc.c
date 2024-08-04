3647f74e0ede70cf01b2dbb097fc6bcf966db0d1
hyperbayse <hyperbayse@gmail.com>
2024-08-04 13:57:06 +0100

    update

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - reallocates a memory block
+ * @ptr:
+ * @old_size:
+ * new_size:
+ * Return: pointer to the newly allocated space
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	int y;
+	void *np;
+
+	if (new_size > old_size)
+	{
+		np = malloc(new_size);
+		if (np == NULL)
+			return (NULL);
+		for (y = 0; y < old_size ; y++)
+			((char *)np)[y] = ((char *)ptr)[y];
+	}
+	if (new_size <= old_size)
+		return (ptr);
+	if (ptr == NULL)
+	{
+		np = malloc(new_size);
+		if (np == NULL)
+			return (NULL);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	free(ptr);
+	return (np);
+}
