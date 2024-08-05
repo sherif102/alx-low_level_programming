4429a51ffe07a9cdaa9d82053cf45223858c90dd
hyperbayse <hyperbayse@gmail.com>
2024-08-05 18:04:57 +0100

    update

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,2 +1,17 @@
-#include "dog.h"
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - dog structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Description: This is a structure defining dog information
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG */
 void init_dog(struct dog *d, char *name, float age, char *owner);
