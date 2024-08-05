b7525d29713a7eb533273324657169db66a9e1d2
hyperbayse <hyperbayse@gmail.com>
2024-08-05 18:00:24 +0100

    update

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include "main.h"
+/**
+ * init_dog - initializes the dog structure
+ * @d: dog structure pasrsed
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
