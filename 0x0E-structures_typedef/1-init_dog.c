9e280e5e9f42a275741d3c617e2abd4501d9866f
hyperbayse <hyperbayse@gmail.com>
2024-08-05 23:17:00 +0100

    update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog - initializes the dog structure
  * @d: dog structure pasrsed
@@ -9,9 +10,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
