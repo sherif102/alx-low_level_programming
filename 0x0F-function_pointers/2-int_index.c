cd9a2f935429b39dddadd1dff9a4400379b4af4e
hyperbayse <hyperbayse@gmail.com>
2024-08-06 20:39:35 +0100

    update

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an integer
+ * @array: supplied array to be searched
+ * @size: array size
+ * @cmp: callback function
+ * Return: Success
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int x;
+
+	if (size <= 0)
+		return (-1);
+
+	if (array != NULL && cmp != NULL)
+	{
+		for (x = 0; x < size; x++)
+		{
+			if (cmp(array[x]) != 0)
+				return (x);
+		}
+	}
+	return (-1);
+}
