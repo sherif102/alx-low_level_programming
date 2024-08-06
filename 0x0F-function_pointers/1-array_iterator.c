eb3a3ee6b226296d2c6b5fc8113f084ba1d15d31
hyperbayse <hyperbayse@gmail.com>
2024-08-06 20:22:41 +0100

    update

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * array_iterator - executes given function on each array element
+ * @array: array passed
+ * @size: array size
+ * @action: callback function
+ * Return: Success
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int x;
+
+	if (action != NULL)
+	{
+		for (x = 0; x < size; x++)
+		{
+			action(array[x]);
+		}
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #ifndef PUTCHAR
 #define PUTCHAR
 char _putchar(char c);
@@ -7,3 +8,8 @@ char _putchar(char c);
 #define PRINT_NAME
 void print_name(char *name, void (*f)(char *));
 #endif /* PRINT_NAME */
+
+#ifndef ARRAY_ITERATOR
+#define ARRAY_ITERATOR
+void array_iterator(int *array, size_t size, void (*action)(int));
+#endif /* ARRAY_ITERATOR */
