bf65c9b349c75107c217defb0244fbf141955789
hyperbayse <hyperbayse@gmail.com>
2024-08-06 06:26:45 +0100

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,7 +3,7 @@
 /**
  * print_dog - prints the data of the dog
  * @d: dog structure
- * Return: void
+ * Return: Success
  */
 void print_dog(struct dog *d)
 {
@@ -11,14 +11,12 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 			d->name = "(nil)";
-		if (d->age == 0)
-			d->age = 0;
+
 		if (d->owner == NULL)
 			d->owner = "(nil)";
+
 		printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner);
 	}
-	else
-		printf("(nil)");
 }
