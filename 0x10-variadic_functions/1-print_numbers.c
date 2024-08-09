052a794b7af2b6865c64ddcb3f06f68e1b52ae2c
hyperbayse <hyperbayse@gmail.com>
2024-08-09 22:27:11 +0100

    update

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,10 +14,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list np;
 
 	va_start(np, n);
+
 	for (x = 0; x < n; x++)
 	{
 		printf("%d", va_arg(np, int));
-		if (x < (n - 1) && separator != NULL)
+
+		if (x < (n - 1) && (separator != NULL))
 			printf("%s", separator);
 	}
 	va_end(np);
