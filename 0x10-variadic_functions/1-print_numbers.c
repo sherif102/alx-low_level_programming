f4900fcee244c11ad734e219d68455d0756330e7
hyperbayse <hyperbayse@gmail.com>
2024-08-09 19:07:37 +0100

    update

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_numbers - print numbers
+ * @separator: what to separate each number
+ * @n: number of integer passed
+ * Return: success
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int x;
+	va_list np;
+
+	va_start(np, n);
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", va_arg(np, int));
+		if (x < (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(np);
+
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,14 @@
 #ifndef _PUTCHAR
 #define _PUTCHAR
-char _putchar(char *);
+char _putchar(char *c);
 #endif /* _PUTCHAR */
 
 #ifndef SUM_THEM_ALL
 #define SUM_THEM_ALL
 int sum_them_all(const unsigned int n, ...);
 #endif /* SUM_THEM_ALL */
+
+#ifndef PRINT_NUMBERS
+#define PRINT_NUMBERS
+void print_numbers(const char *separator, const unsigned int n, ...);
+#endif /* PRINT_NUMBERS */
