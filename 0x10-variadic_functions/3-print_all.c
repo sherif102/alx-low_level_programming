2a6c47041aad0f9c3d44909674068033c90674f8
hyperbayse <hyperbayse@gmail.com>
2024-08-10 15:20:56 +0100

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,55 +10,41 @@
 void print_all(const char * const format, ...)
 {
 	char c;
-	int i, t;
+	int i;
 	double f;
-	char *s;
+	char *s, *es = "\n";
 	int x = 0;
-
 	va_list ap;
 
 	va_start(ap, format);
-
-	while (*(format + x) != '\0')
+	while (format && format[x] != '\0')
 	{
-		t = *(format + x);
-		if ((t == 'c' || t == 'i' || t == 'f' || t == 's'))
+		switch (format[x])
 		{
-			switch (*(format + x))
-			{
-				case 'c':
-				{
-					c = (char) va_arg(ap, int);
-					printf("%c", c);
-					break;
-				}
-				case 'i':
-				{
-					i = va_arg(ap, int);
-					printf("%d", i);
-					break;
-				}
-				case 'f':
-				{
-					f = va_arg(ap, double);
-					printf("%f", f);
-					break;
-				}
-				case 's':
-				{
-					s = va_arg(ap, char *);
-					printf("%s", (s != NULL) ? s : "(nil)");
-					break;
-				}
-				default:
-					break;
-			}
-
-			if (*(format + (x + 1)) != '\0')
-				printf(", ");
+			case 'c':
+				c = (char) va_arg(ap, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				i = va_arg(ap, int);
+				printf("%d", i);
+				break;
+			case 'f':
+				f = va_arg(ap, double);
+				printf("%f", f);
+				break;
+			case 's':
+				s = va_arg(ap, char *);
+				printf("%s", (s != NULL) ? s : "(nil)");
+				break;
+			default:
+				x++;
+				continue;
 		}
+		if (*(format + (x + 1)) != '\0')
+			printf(", ");
 		x++;
 	}
+	printf("%s", (format != NULL) ? es : "(nil)\n");
 	va_end(ap);
-	printf("\n");
 }
