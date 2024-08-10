98914fcfd83a777c9d51bb8b4a72dd6cd324846c
hyperbayse <hyperbayse@gmail.com>
2024-08-10 08:55:08 +0100

    update

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,40 +1,33 @@
-//for (x = 0; x < n; x++)
-//	{
-//		z = va_arg(np, int);
-//		if (z < 0)
-//		{
-//			z = -(z);
-//			_putchar('-');
-//		}
-//		if (z > 999)
-//		{
-//			_putchar((z / 1000) + '0');
-//			_putchar(((z / 100) % 10) + '0');
-//			_putchar(((z / 10) % 10) + '0');
-//			_putchar((z % 10) + '0');
-//		}
-//		else if (z > 99)
-//		{
-//			_putchar(((z / 100) % 10) + '0');
-//			_putchar(((z / 10) % 10) + '0');
-//			_putchar((z % 10) + '0');
-//		}
-//		else if (z > 9)
-//		{
-//			_putchar(((z / 10) % 10) + '0');
-//			_putchar((z % 10) + '0');
-//		}
-//		else
-//			_putchar(z + '0');
-//
-//		if ((x < (n - 1)) && (separator != NULL))
-//		{
-//			y = 0;
-//			while (separator[y] != '\0')
-//			{
-//				_putchar(separator[y]);
-//				y++;
-//			}
-//		}
-//
-//	}
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_strings - print strings
+ * @separator: word separator
+ * @n: number of argument parsed
+ * Return: success
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int x;
+	char *z;
+	va_list sp;
+
+	va_start(sp, n);
+
+	for (x = 0; x < n; x++)
+	{
+		z = va_arg(sp, char *);
+
+		if (z == NULL)
+			printf("(nil)");
+		else
+			printf("%s", z);
+
+		if ((x < (n - 1)) && (separator != NULL))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(sp);
+}
