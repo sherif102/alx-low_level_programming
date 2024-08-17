6cac6a58c9b85a14a202f464fd9702e8cf1dca72
hyperbayse <hyperbayse@gmail.com>
2024-08-18 00:09:59 +0100

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	for (i = 0; i < (idx - 1); i++)
 	{
 		temp = temp->next;
-		if (temp->next == NULL)
+		if (temp == NULL)
 			return (NULL);
 	}
 	new_node->next = temp->next;
