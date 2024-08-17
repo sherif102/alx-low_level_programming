4dcfa5100447b32483ddbf1ef9dd03b0a7778da9
hyperbayse <hyperbayse@gmail.com>
2024-08-17 02:36:52 +0100

    update

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_listint - prints all element in a list
+ * @h function parsed
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	const listint_t *temp = h;
+
+	while (temp != NULL)
+	{
+		printf("%u\n", temp->n);
+		temp = temp->next;
+		n++;
+	}
+	return (n);
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,30 @@
+#ifndef SIZE_T
+#define SIZE_T
+#include <stddef.h>
+#endif /* SIZE_T */
+
+#ifndef LISTINT_S
+#define LISTINT_S
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+#endif /* LISTINT_S */
+
+#ifndef _PUTCHAR
+#define _PUTCHAR
+int _putchar(char c);
+#endif /* _PUTCHAR */
+
+#ifndef PRINT_LISTINT
+#define PRINT_LISTINT
+size_t print_listint(const listint_t *h);
+#endif /* PRINT_LISTINT */
