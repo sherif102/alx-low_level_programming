ad3bbe4eac967f7bec8eb7063b0b4b5ba771a164
hyperbayse <hyperbayse@gmail.com>
2024-08-18 08:37:15 +0100

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	size_t i;
+	size_t i = 1;
 	listint_t *temp = *head;
 	listint_t *new_node = (listint_t *)malloc(sizeof(listint_t));
 
@@ -21,9 +21,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (*head == NULL)
 		return (NULL);
 
-	while (i < (idx - 1))
+	while (i < idx)
 	{
-		if (temp->next == NULL)
+		if ((temp->next == NULL) && (i != (idx - 1)))
 			return (NULL);
 		temp = temp->next;
 		i++;
