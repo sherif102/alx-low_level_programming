9329131a0f420ce005007674c998728cc8b7f078
hyperbayse <hyperbayse@gmail.com>
2024-08-22 10:02:11 +0100

    update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,21 +12,26 @@ size_t print_listint_safe(const listint_t *head)
 	const listint_t *temp = head;
 	const listint_t *fast = head;
 
-	while (temp != NULL && head != NULL)
+	while (temp != NULL)
 	{
+		node_count++;
 		printf("[%p] %d\n", (void *)temp, temp->n);
 		temp = temp->next;
 
 		if (fast != NULL)
 		{
 			fast = fast->next->next;
+			if (fast == NULL)
+			{
+				printf("-> [%p] %d\n", (void *)temp, temp->n);
+				exit(98);
+			}
 			if (fast == temp || fast == head)
 			{
 				printf("-> [%p] %d\n", (void *)fast, fast->n);
 				exit(98);
 			}
 		}
-		node_count++;
 	}
 	return (node_count);
 }
