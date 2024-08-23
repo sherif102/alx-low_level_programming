94a11578598508cd5ce48d44d2c6bca209443b85
hyperbayse <hyperbayse@gmail.com>
2024-08-23 08:05:10 +0100

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,8 +1,41 @@
-/*
- * 0-binary_to_uint.c
- *
- *  Created on: Aug 22, 2024
- *      Author: hyper
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * binary_to_uint - converts binary number to unsigned integer
+ * @b: binary parsed
+ * Return: converted number or 0 if otherwise
  */
+unsigned int binary_to_uint(const char *b)
+{
+	size_t count = 0;
+	size_t i = 0;
+	size_t m = 1;
+	size_t c;
+	size_t result = 0;
 
+	if (b == NULL)
+		return (0);
 
+	while (b[count] != '\0')
+		count++;
+
+	while (b[i] != '\0')
+	{
+		if (b[i] > 49 || b[i] < 48)
+			return (0);
+		c = count - 1;
+
+		while (c > 0)
+		{
+			m = m * 2;
+			c--;
+		}
+		m = m * (b[i] - 48);
+		result = result + m;
+		m = 1;
+		i++;
+		count--;
+	}
+	return (result);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,4 +1,4 @@
-#ifndef
-#define
-
-#endif /*  */
+#ifndef BINARY_TO_UINT
+#define BINARY_TO_UINT
+unsigned int binary_to_uint(const char *b);
+#endif /* BINARY_TO_UINT */
