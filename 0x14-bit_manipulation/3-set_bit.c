2dd78a7d5ddcab889a251be6587bbb666d1c8f05
hyperbayse <hyperbayse@gmail.com>
2024-08-26 10:31:32 +0100

    update

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * set_bit - set bit value
+ * @n: value parsed
+ * @index: index to set
+ * Return: 1 if it worked and -1 if otherwise
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int x = 1;
+
+	if (index > 63)
+		return (-1);
+
+	while (index > 0)
+	{
+		x = x * 2;
+		index--;
+	}
+
+	*n = *n | x;
+
+	return (*n);
+}
