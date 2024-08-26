2e406488cc670a76bfa23fae0fd91d42803938ac
hyperbayse <hyperbayse@gmail.com>
2024-08-26 15:24:17 +0100

    update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,8 +12,11 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > 63)
 		return (-1);
 
+
+
 	if (x >= (1 << index))
-		*n = *n ^ 1 << index;
+		if ((*n >> index) % 2 == 1)
+			*n = *n ^ 1 << index;
 
 	return (1);
 }
