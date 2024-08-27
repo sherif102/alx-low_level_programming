4c0c59fc70e576842d2088c9797888ff810a2ecd
hyperbayse <hyperbayse@gmail.com>
2024-08-27 18:48:45 +0100

    update

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * read_textfile - read a text files and print to POSIX STDIN
+ * @filename: filename
+ * @letters: number of letters to read
+ * Return: number of letter able to read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t bytes_read;
+
+	int fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+		return (0);
+
+	write(STDOUT_FILENO, buffer, bytes_read);
+
+	return (bytes_read);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,24 @@
+#ifndef _PUTCHAR
+#define _PUTCHAR
+int _putchar(char c);
+#endif /* _PUTCHAR */
+
+#ifndef FCNTL
+#define FCNTL
+#include <fcntl.h>
+#endif /* FCNTL */
+
+#ifndef STDDEF
+#define STDDEF
+#include <stddef.h>
+#endif /* STDDEF */
+
+#ifndef UNISTD
+#define UNISTD
+#include <unistd.h>
+#endif /* UNISTD */
+
+#ifndef READ_TEXTFILE
+#define READ_TEXTFILE
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif /* READ_TEXTFILE */
