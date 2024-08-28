0eca6c8211c2f21b33d55e78b48f44554ab90a93
hyperbayse <hyperbayse@gmail.com>
2024-08-28 11:53:03 +0100

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,28 +13,40 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
-	ssize_t bytes_read;
-	int fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-		return (0);
+	ssize_t bytes_read = 0;
+	ssize_t bytes_write = 0;
+	int fd;
 
 	if (filename == NULL)
 		return (0);
 
 	buffer = malloc(letters);
-
 	if (buffer == NULL)
 		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
+		free(buffer);
 		close(fd);
 		return (0);
 	}
 
-	write(STDOUT_FILENO, buffer, bytes_read);
-
-	return (bytes_read);
+	bytes_write = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_write == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	close(fd);
+	free(buffer);
+	return (bytes_write);
 }
