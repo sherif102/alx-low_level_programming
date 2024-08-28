710fba7fc640f7a9a5341df722a514f19bbd264b
hyperbayse <hyperbayse@gmail.com>
2024-08-28 23:10:48 +0100

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,42 +10,34 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	char *buffer;
 	ssize_t bytes_written = 0;
 	ssize_t i = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[i] != '\0')
-		i++;
-	i++;
-
-	buffer = malloc(i);
-	if (buffer == NULL)
-		return (-1);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT, 0600);
+		if (fd == -1)
+			return (-1);
+		return (1);
+	}
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
-	{
-		free(buffer);
 		return (-1);
-	}
 
-	if (text_content[0] == 'N' && text_content[1] == 'U')
-	{
-		bytes_written = write(fd, text_content, 0);
-	}
-	else
-	{
-		bytes_written = write(fd, text_content, i);
-	}
+	while (text_content[i] != '\0')
+		i++;
+
+	bytes_written = write(fd, text_content, i);
 	if (bytes_written == -1)
 	{
-		free(buffer);
 		close(fd);
 		return (-1);
 	}
 
+	close(fd);
 	return (1);
 }
