fe08980647cef223e2a776c1ce33822fd059c197
hyperbayse <hyperbayse@gmail.com>
2024-08-28 11:30:55 +0100

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,7 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1 || bytes_read < letters)
+	if (bytes_read == -1)
 	{
 		close(fd);
 		return (0);
