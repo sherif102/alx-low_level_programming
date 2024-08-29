ecdc9eb7246f4a2a7b3a29a5c8d24959518a0a0d
hyperbayse <hyperbayse@gmail.com>
2024-08-29 12:31:15 +0100

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,33 +5,28 @@
 #include <unistd.h>
 /**
  * main - copy file from and to
- * @argc: number of argument supplied
- * @argv: argument parsed
+ * @c: number of argument supplied
+ * @v: argument parsed
  * Return: success
  */
 
-
-int main(int argc, char **argv)
+int main(int c, char **v)
 {
 	int fd1, fd2;
 	ssize_t read1, write2 = 0;
 	char buffer[1024];
 
-	if (argc != 3)
-	{
+	if (c != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	}
 
-	fd1 = open(argv[1], O_RDONLY);
+	fd1 = open(v[1], O_RDONLY);
 	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", v[1]), exit(98);
 
-	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd2 = open(v[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", v[2]);
 		close(fd1);
 		exit(99);
 	}
@@ -41,30 +36,18 @@ int main(int argc, char **argv)
 		write2 = write(fd2, buffer, read1);
 		if (write2 == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", v[2]);
 			close(fd1);
 			close(fd2);
 			exit(99);
 		}
 	}
 	if (read1 == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		close(fd1);
-		close(fd2);
-		exit(98);
-	}
-
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", v[1]), exit(98);
 	if (close(fd1) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %i\n", fd1);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd1), exit(100);
 	if (close(fd2) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %i\n", fd2);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd2), exit(100);
 
 	return (0);
 }
