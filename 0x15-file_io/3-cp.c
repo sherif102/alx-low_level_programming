5254d3df2c0ef1012563d30ccf70f94fc3456ed4
hyperbayse <hyperbayse@gmail.com>
2024-08-29 11:43:34 +0100

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,16 +2,14 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
 
 
 
 int main(int argc, char **argv)
 {
-	int fd1;
-	int fd2;
-	ssize_t read1 = 0;
-	ssize_t write2 = 0;
-	ssize_t i = 0;
+	int fd1, fd2;
+	ssize_t read1, write2 = 0;
 	char buffer[1024];
 
 	if (argc != 3)
@@ -29,47 +27,47 @@ int main(int argc, char **argv)
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
 	{
-		dprintf(fd1, "Error: Can't read from the %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from the %s\n", argv[1]);
 		exit(98);
 	}
 
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
-		dprintf(fd2, "Error: Can't write to %s\n", argv[2]);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		close(fd1);
 		exit(99);
 	}
 
 	while ((read1 = read(fd1, buffer, 1024)) > 0)
 	{
-		while (buffer[i] != '\0')
-			i++;
-
-		write2 = write(fd2, buffer, i);
+		write2 = write(fd2, buffer, read1);
 		if (write2 == -1)
 		{
-			dprintf(fd2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			close(fd1);
+			close(fd2);
 			exit(99);
 		}
 	}
 	if (read1 == -1)
 	{
-		dprintf(fd1, "Error: Can't read from the %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from the %s\n", argv[1]);
+		close(fd1);
+		close(fd2);
 		exit(98);
 	}
 
-	if (!close(fd2))
+	if (close(fd1) == -1)
 	{
-		dprintf(fd2, "Error: Can't close fd %i\n", fd2);
+		dprintf(2, "Error: Can't close fd %i\n", fd1);
 		exit(100);
 	}
-	if (!close(fd1))
+	if (close(fd2) == -1)
 	{
-		dprintf(fd1, "Error: Can't close fd %i\n", fd1);
+		dprintf(2, "Error: Can't close fd %i\n", fd2);
 		exit(100);
 	}
 
-	close(fd2);
-	close(fd1);
 	return (0);
 }
