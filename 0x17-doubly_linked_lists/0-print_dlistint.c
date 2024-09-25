84368d4267a0beb14a5ffeb06d14cd521f15fcda
hyperbayse <hyperbayse@gmail.com>
2024-09-25 09:30:26 +0100

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "lists.h"
 
-/**
+/*
  * print_dlistint - print elements in a doubly linked list
  * @h: head parsed
  * Return: number of elements there
@@ -9,18 +9,17 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t n = 0;
-    const dlistint_t *temp;
+        size_t n = 0;
+        const dlistint_t *temp;
 
-    if (h == NULL)
+        if (h == NULL)
+                return (n);
+
+        temp = h;
+        while (temp != NULL) {
+                printf("%d\n", temp->n);
+                temp = temp->next;
+                n++;
+        }
         return (n);
-
-    temp = h;
-    while (temp != NULL)
-    {
-        printf("%d\n", temp->n);
-        temp = temp->next;
-        n++;
-    }
-    return (n);
 }
