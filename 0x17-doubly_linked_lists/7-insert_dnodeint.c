51eb3dc73694b9066a0b92c173ffd3800ddcba2f
hyperbayse <hyperbayse@gmail.com>
2024-09-25 22:12:07 +0100

    update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,39 +17,38 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx < x)
 		return (NULL);
 
-	if (idx == x || *h == NULL)
+	if (idx == x)
 	{
 		temp = add_dnodeint(h, n);
 		return (temp);
 	}
-
 	node = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (node == NULL)
-		return (NULL);
-
-	temp = *h;
-	while (temp != NULL && idx > 0)
 	{
-		idx--;
+		free(node);
+		return (NULL);
+	}
+	temp = *h;
+	x++;
+	while (temp != NULL && x < idx)
+	{
+		x++;
 		temp = temp->next;
 
-		if (temp == NULL && idx > (x + 1))
-			return (NULL);
-
-		if (idx == (x + 1) && temp != NULL)
-		{
-			node->n = n;
-			node->next = temp->next;
-			temp->next = node;
-			node->prev = temp;
-			return (node);
-		}
-
-		if (temp == NULL && idx == (x + 1))
+		if (temp == NULL && x == (idx - 1))
 		{
 			temp = add_dnodeint_end(h, n);
 			return (temp);
 		}
+		if (temp == NULL)
+		{
+			free(node);
+			return (NULL);
+		}
 	}
-	return (NULL);
+	node->n = n;
+	node->next = temp->next;
+	temp->next = node;
+	node->prev = temp;
+	return (node);
 }
