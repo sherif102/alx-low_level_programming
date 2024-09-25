a7b47c5eff1e097edfe0b2a1c14c96fd7b1aa187
hyperbayse <hyperbayse@gmail.com>
2024-09-25 12:57:17 +0100

    update

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,7 +11,7 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *temp, *node;
-	
+
 	node = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (node == NULL)
 		return (NULL);
