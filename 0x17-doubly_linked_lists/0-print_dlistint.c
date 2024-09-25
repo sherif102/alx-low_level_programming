b21eb23a972cddd032ceb62675dbaf72d00e32ab
hyperbayse <hyperbayse@gmail.com>
2024-09-25 09:09:28 +0100

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,9 +11,10 @@ size_t print_dlistint(const dlistint_t *h)
 {
     size_t n = 0;
     const dlistint_t *temp;
-    
+
     if (h == NULL)
         return (n);
+
     temp = h;
     while (temp != NULL)
     {
