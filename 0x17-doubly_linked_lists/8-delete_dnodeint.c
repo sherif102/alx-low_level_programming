9bae865414bbd6472b28da44d1d3f69e7e2911fb
hyperbayse <hyperbayse@gmail.com>
2024-09-26 08:26:01 +0100

    update

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - deletes node at a given index
+ * @head: parsed head
+ * @index: index to be deleted
+ * Return: 1 if success or -1 if failed
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp, *node;
+	unsigned int x = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	
+	if (index == x)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(temp);
+		return (1);
+	}
+
+	temp = *head;
+	while (temp != NULL && x < index - 1)
+	{
+		temp = temp->next;
+		x++;
+	}
+
+	if (temp == NULL || temp->next == NULL)
+		return (-1);
+	node = temp->next;
+	temp->next = node->next;
+	if (node->next != NULL)
+		node->next->prev = temp;
+	free(node);
+	return (1);
+}
