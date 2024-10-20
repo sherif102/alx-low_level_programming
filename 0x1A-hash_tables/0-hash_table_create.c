475f1f1dd127eae3cace9175207467b2dbd882eb
hyperbayse <hyperbayse@gmail.com>
2024-10-21 00:32:31 +0100

    update

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the table to be created
+ * Return: pointer to the created hash table or NULL if failed
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+	hash_node_t **node = NULL;
+	hash_table_t *table = malloc(sizeof(hash_table_t) * size);
+
+	if (table == NULL)
+		return (NULL);
+
+	for (; i < size; i++)
+	{
+		table[i].array = node;
+		i++;
+	}
+
+	return (table);
+}
