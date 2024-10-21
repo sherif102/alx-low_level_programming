e091a2e13aec84fc6b3582481ffdac992fe3ab7e
hyperbayse <hyperbayse@gmail.com>
2024-10-21 01:20:57 +0100

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
 	hash_node_t **node = NULL;
-	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_table_t) * size);
+	hash_table_t *table = (hash_table_t *)malloc(sizeof(hash_node_t) * size);
 
 	if (table == NULL)
 		return (NULL);
