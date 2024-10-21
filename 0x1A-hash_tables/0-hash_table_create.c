595de5bf6735a4a9b497032ec3bb520cb632fb92
hyperbayse <hyperbayse@gmail.com>
2024-10-21 10:17:14 +0100

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -21,7 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	table->array = calloc(size, sizeof(hash_node_t));
+	table->array = calloc(size, sizeof(hash_node_t *));
 
 	if (table->array == NULL)
 	{
