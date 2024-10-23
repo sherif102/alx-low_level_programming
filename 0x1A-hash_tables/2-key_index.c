290a9a2279cd2ed8cc3f3d06bdd4d9aaf1a25632
hyperbayse <hyperbayse@gmail.com>
2024-10-23 08:40:33 +0100

    update

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - generates the index of a key
+ * @key: the key to lookup for
+ * @size: hash table size
+ * Return: index generated
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hashr = hash_djb2(key);
+	return (hashr % size);
+}
