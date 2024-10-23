0521776cc9330b5269766a91dd9dfcf6b4cd5f9c
hyperbayse <hyperbayse@gmail.com>
2024-10-23 14:12:32 +0100

    update

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value from hash table
+ * @ht: hash table
+ * @key: key to use
+ * Return: value stored in the key or NULL if not found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	char *value;
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+
+	if (ht->array[index] == NULL)
+		return (NULL);
+
+	value = strdup(ht->array[index]->value);
+
+	if (value == NULL)
+	{
+		free(value);
+		return (NULL);
+	}
+	return (value);
+}
