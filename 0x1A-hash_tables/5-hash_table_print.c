aa4d08d8d1341a915e1b484c9a2adb3828aaa839
hyperbayse <hyperbayse@gmail.com>
2024-10-24 14:30:35 +0100

    update

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,48 +3,64 @@
 /**
  * hash_table_print - prints the hash table
  * @ht: hash table
- * @ti: table iterator
- * @li: list iterator
  * Return: success
  */
 
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int ti;
-	hash_node_t *li, *temp;
+	unsigned long int ti, oi = 0;
+	int bc = 1;
+	hash_node_t *temp;
 	char *output;
 
-	if (ht == NULL || ht->array == NULL)
-	{
-		printf("{}\n");
+	if (ht == NULL)
 		return;
-	}
 
-	ti = 0;
-	output = malloc(2048);
-	while (ht->array[ti])
+	output = malloc(BUFFER);
+	if (output == NULL)
+		return;
+
+	output[oi++] = '{';
+	for (ti = 0; ti < ht->size; ti++)
 	{
-		li = ht->array[ti];
-		if (li != NULL)
+		if (ht->array[ti] != NULL && ht->array[ti - 1] != NULL)
+			datacopy(output, "', ", &oi, &bc);
+		if (ht->array[ti] != NULL)
 		{
-			temp = li;
+			temp = ht->array[ti];
 			while (temp != NULL)
 			{
-				strcat(output, "\'");
-				strcat(output, temp->key);
-				strcat(output, "\'");
-
-				strcat(output, ": ");
-				strcat(output, "\'");
-				strcat(output, temp->value);
-				strcat(output, "\'");
-				if (temp->next != NULL)
-					strcat(output, ", ");
-
+				output[oi++] = '\'';
+				datacopy(output, temp->key, &oi, &bc);
+				datacopy(output, "': '", &oi, &bc);
+				datacopy(output, temp->value, &oi, &bc);
 				temp = temp->next;
+				if (temp != NULL)
+					datacopy(output, "', ", &oi, &bc);
 			}
 		}
-		ti++;
 	}
+	output[oi] = '}';
 	printf("%s\n", output);
+}
+
+/**
+ * datacopy - copy input and append to output
+ * it reallocate memory if needed
+ * @output: copy to destination
+ * @input: copy from source
+ * @oi: output iterator, it is the number of all char in output
+ * @bc: buffer counter, 1024 * bc
+ * Return: success
+ */
+void datacopy(char *output, char *input, unsigned long int *oi, int *bc)
+{
+	unsigned long int vi;
+	int len = strlen(output) + 15;
+
+	if (len >= BUFFER * (*bc))
+		output = realloc(output, BUFFER * (++(*bc)));
+
+	for (vi = 0; vi < strlen(input); vi++)
+		output[(*oi)++] = input[vi];
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,8 @@
 #ifndef __HASH_TABLE
 #define __HASH_TABLE
 
+#define BUFFER 1024
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -38,4 +40,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void datacopy(char *output, char *input, unsigned long int *oi, int *bc);
 #endif /* __HASH_TABLE */
