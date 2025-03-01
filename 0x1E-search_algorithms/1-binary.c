b84a5d2268d160824ffb27b3cf17819922cf147a
hyperbayse <hyperbayse@gmail.com>
2025-03-01 10:12:24 +0100

    binary search algorithm

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for value in binary data
+ * @array: array to be searched
+ * @size: size of the array
+ * @value: value to be searched
+ * Return: success
+ */
+
+ int binary_search(int *array, size_t size, int value)
+ {
+	int L, R, m;
+
+	L = 0;
+	R = size - 1;
+	m = (L + R) / 2;
+
+	while (L <= R)
+	{
+		array_printer(array, L, R);
+		if (array[m] > value)
+			R = m - 1;
+		else
+			L = m + 1;
+		m = (L + R) / 2;
+		if (array[m] == value)
+			return (m);
+	}
+	return (-1);
+ }
+
+
+ void array_printer(int *array, int start, int stop)
+ {
+	int counter = start;
+
+	printf("Searching in array: ");
+	while (counter < stop)
+		printf("%d, ", array[counter++]);
+	printf("%d\n", array[stop]);		
+ }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,7 @@
 #include <stdio.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void array_printer(int *array, int start, int stop);
 
 #endif /**SEARCH_ALGOS */
